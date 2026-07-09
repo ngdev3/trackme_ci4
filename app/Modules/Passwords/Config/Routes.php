a9f78c561b51608c5ad64001c5679a281cdc4600
Er Rajat Gupta <45349777+ngdev3@users.noreply.github.com>
2026-07-09 08:21:27 +0530

    Update Routes.php

--- app/Modules/Passwords/Config/Routes.php
+++ app/Modules/Passwords/Config/Routes.php
@@ -6,14 +6,16 @@ use CodeIgniter\Router\RouteCollection;
 
 $routes->group('passwords', ['namespace' => 'Modules\Passwords\Controllers'], static function (RouteCollection $routes) {
     $routes->get('/', 'PasswordController::index', ['filter' => 'permission:passwords,view']);
-    $routes->get('view/(:num)', 'PasswordController::view/$1', ['filter' => 'permission:passwords,view']);
-    $routes->get('reveal/(:num)', 'PasswordController::reveal/$1', ['filter' => 'permission:passwords,view']);
+    $routes->get('list', 'PasswordController::index', ['filter' => 'permission:passwords,view']);
+    $routes->get('view/(:segment)', 'PasswordController::view/$1', ['filter' => 'permission:passwords,view']);
+    $routes->get('reveal/(:segment)', 'PasswordController::reveal/$1', ['filter' => 'permission:passwords,view']);
 
+    $routes->get('add', 'PasswordController::create', ['filter' => 'permission:passwords,add']);
     $routes->get('create', 'PasswordController::create', ['filter' => 'permission:passwords,add']);
     $routes->post('store', 'PasswordController::store', ['filter' => 'permission:passwords,add']);
 
-    $routes->get('edit/(:num)', 'PasswordController::edit/$1', ['filter' => 'permission:passwords,edit']);
-    $routes->post('update/(:num)', 'PasswordController::update/$1', ['filter' => 'permission:passwords,edit']);
+    $routes->get('edit/(:segment)', 'PasswordController::edit/$1', ['filter' => 'permission:passwords,edit']);
+    $routes->post('update/(:segment)', 'PasswordController::update/$1', ['filter' => 'permission:passwords,edit']);
 
-    $routes->post('delete/(:num)', 'PasswordController::delete/$1', ['filter' => 'permission:passwords,delete']);
+    $routes->post('delete/(:segment)', 'PasswordController::delete/$1', ['filter' => 'permission:passwords,delete']);
 });
